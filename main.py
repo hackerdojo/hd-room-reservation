982ec5dff49946da57b72df41a162028768d7584
Waleed Abdulla <waleed@ninua.com>
2014-05-31 18:40:04 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+import datetime
+import webapp2
+import jinja2
+from google.appengine.ext import ndb
+from google.appengine.api import users
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+#-- Data Model ----------------------------------------------------------------
+
+class Slot(ndb.Model):
+  user = ndb.UserProperty()
+  room = ndb.StringProperty(default="4c")  # we have one room only for now
+  start = ndb.DateTimeProperty()  # Start of reserved slot. Each slot is 30m.
+  
+
+#-- Handlers ------------------------------------------------------------------
+
+class HomePage(webapp2.RequestHandler):
+  def get(self):
+    # Get logged in user.
+    user = users.get_current_user()
+    
+    # If the user is not logged in, ask them to.
+    if not user:
+      html = '<a href="%s">Sign In</a>' % users.create_login_url('/')
+      self.response.write(html)
+      return
+
+    # As an example, create and save one slot that starts now.
+    slot = Slot(user=user, start=datetime.datetime.now())
+    slot.put()
+    
+    # Load last 10 slots.
+    slots = Slot.query().order(-Slot.start).fetch(10)
+    
+    # Set values to pass to HTML template.
+    template_values = {
+      'user': user,
+      'slots': slots,
+    }
+    template = JINJA_ENVIRONMENT.get_template('index.html')
+    self.response.write(template.render(template_values))
+
+# Route URL Requests to handlers.
+app = webapp2.WSGIApplication([
+  # Home page
+  ('/?', HomePage),
+  ], debug=True)
+
